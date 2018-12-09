f970f106955d7ec2011852d565be9ee84bd55707
archisha.m <architen100@gmail.com>
2018-12-09 16:00:05 -0600

    Implement API

--- app/src/main/java/com/example/archi/crimefinder/MainActivity.java
+++ app/src/main/java/com/example/archi/crimefinder/MainActivity.java
@@ -2,72 +2,129 @@ package com.example.archi.crimefinder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.view.View;
 import android.content.Intent;
 import android.widget.EditText;
 import android.widget.TextView;
+import com.android.volley.toolbox.Volley;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.Map;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
     private String longitude;
     private String latitude;
+    private String start_Date;
+    private String end_Date;
+    private static RequestQueue requestQueue;
+    private static final String TAG = "MainActivity";
+    private String crimes = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestQueue = Volley.newRequestQueue(this);
         setContentView(R.layout.activity_main);
 
-        Button radius1mi = (Button) findViewById(R.id.radius1mi);
-        radius1mi.setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View v) {
-                 Intent startIntent = (new Intent(MainActivity.this, ResultsActivity.class));
+        Button sub_button = (Button) findViewById(R.id.sub_button);
+        sub_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = (new Intent(MainActivity.this, ResultsActivity.class));
 
-                 startIntent.putExtra("com.example.archi.crimefinder.RADIUS", "1");
-                 EditText enterLatitude = (EditText) findViewById(R.id.enterLatitude);
-                 EditText enterLongitude = (EditText) findViewById(R.id.enterLongitude);
-                 latitude = enterLatitude.getText().toString();
-                 longitude = enterLongitude.getText().toString();
-                 startIntent.putExtra("com.example.archi.crimefinder.LATITUDE", latitude);
-                 startIntent.putExtra("com.example.archi.crimefinder.LONGITUDE", longitude);
-                 startActivity(startIntent);
+                EditText enterLatitude = (EditText) findViewById(R.id.enterLatitude);
+                EditText enterLongitude = (EditText) findViewById(R.id.enterLongitude);
+                latitude = enterLatitude.getText().toString();
+                longitude = enterLongitude.getText().toString();
+                startIntent.putExtra("com.example.archi.crimefinder.LATITUDE", latitude);
+                startIntent.putExtra("com.example.archi.crimefinder.LONGITUDE", longitude);
 
-             }
-         }
-        );
-        Button radius5mi = (Button) findViewById(R.id.radius5mi);
-        radius5mi.setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View v) {
-                 Intent startIntent = (new Intent(MainActivity.this, ResultsActivity.class));
-                 startIntent.putExtra("com.example.archi.crimefinder.RADIUS", "5");
-                 EditText enterLatitude = (EditText) findViewById(R.id.enterLatitude);
-                 EditText enterLongitude = (EditText) findViewById(R.id.enterLongitude);
-                 latitude = enterLatitude.getText().toString();
-                 longitude = enterLongitude.getText().toString();
-                 startIntent.putExtra("com.example.archi.crimefinder.LATITUDE", latitude);
-                 startIntent.putExtra("com.example.archi.crimefinder.LONGITUDE", longitude);
-                 startActivity(startIntent);
-             }
-         }
-        );
-        Button radius10mi = (Button) findViewById(R.id.radius10mi);
-        radius10mi.setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View v) {
-                 Intent startIntent = (new Intent(MainActivity.this, ResultsActivity.class));
-                 startIntent.putExtra("com.example.archi.crimefinder.RADIUS", "10");
-                 EditText enterLatitude = (EditText) findViewById(R.id.enterLatitude);
-                 EditText enterLongitude = (EditText) findViewById(R.id.enterLongitude);
-                 latitude = enterLatitude.getText().toString();
-                 longitude = enterLongitude.getText().toString();
-                 startIntent.putExtra("com.example.archi.crimefinder.LATITUDE", latitude);
-                 startIntent.putExtra("com.example.archi.crimefinder.LONGITUDE", longitude);
-                 startActivity(startIntent);
-             }
-         }
-        );
+                EditText start_date = (EditText) findViewById(R.id.start_Date);
+                EditText end_date = (EditText) findViewById(R.id.end_Date);
+                start_Date = start_date.getText().toString();
+                end_Date = end_date.getText().toString();
+                startIntent.putExtra("com.example.archi.crimefinder.START", start_Date);
+                startIntent.putExtra("com.example.archi.crimefinder.END", end_Date);
+
+                startIntent.putExtra("com.example.archi.crimefinder.CRIMES", crimes);
+
+                startActivity(startIntent);
+
+            }
+        });
     }
+    void startAPICall() {
+        try {
+            String[] startDateStr = start_Date.split("/");
+            int[] startDate = new int[3];
+            for (int i = 0; i < 3; i++) {
+                startDate[i] = Integer.parseInt(startDateStr[i]);
+            }
+            String[] endDateStr = end_Date.split("/");
+            int[] endDate = new int[3];
+            for (int i = 0; i < 3; i++) {
+                endDate[i] = Integer.parseInt(endDateStr[i]);
+            }
+            String url = "https://jgentes-Crime-Data-v1.p.mashape.com/crime?enddate=" + endDate[0] +
+                    "%2F" + endDate[1] + "%2F" + endDate[2] + "&lat=" + Double.parseDouble(latitude) +
+                    "&long=" + Double.parseDouble(latitude) + "&startdate=" + startDate[0] + "%2F" +
+                    startDate[1] + "%2F" + startDate[2];
 
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                    Request.Method.GET,
+                    url,
+                    null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(final JSONObject response) {
+                            try {
+                                Log.d(TAG, response.toString());
+                                JSONArray crimeArray = response.getJSONArray(response.toString());
+                                String description = "";
+                                String datetime = "";
+                                String location = "";
+                                for (int count = 0; count < crimeArray.length(); count++) {
+                                    JSONObject obj = crimeArray.getJSONObject(count);
+                                    description = obj.getString("description");
+                                    datetime = obj.getString("datetime");
+                                    location = obj.getString("location");
+                                }
+                                crimes += "/n" + description + " at " + datetime + " at location: "
+                                        + location;
+                            } catch (Exception e1) {
+                                e1.printStackTrace();
+                            }
+
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(final VolleyError error) {
+                            Log.w(TAG, error.toString());
+                        }
+                    }) {
+                @Override
+                public Map<String, String> getHeaders() {
+                    Map<String, String> params = new HashMap<String, String>();
+                    params.put("X-Mashape-Key", "AIzaSyDpLSvpu8_KDhIDbfI9pHhAlEEKA8Hup0g");
+                    params.put("Accept", "application/json");
+                    return params;
+                }
+            };
+            requestQueue.add(jsonObjectRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 
 }
--- app/src/main/java/com/example/archi/crimefinder/ResultsActivity.java
+++ app/src/main/java/com/example/archi/crimefinder/ResultsActivity.java
@@ -8,6 +8,9 @@ import android.content.Intent;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public class ResultsActivity extends AppCompatActivity {
 
     @Override
@@ -16,15 +19,25 @@ public class ResultsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_results);
 
         Intent intent = getIntent();
-        String getRadius = intent.getStringExtra("com.example.archi.crimefinder.RADIUS");
-        int radius = Integer.parseInt(getRadius);
         String getLat = intent.getStringExtra("com.example.archi.crimefinder.LATITUDE");
         Double latitude = Double.parseDouble(getLat);
         String getLong = intent.getStringExtra("com.example.archi.crimefinder.LONGITUDE");
         Double longitude = Double.parseDouble(getLong);
+        String getStart = intent.getStringExtra("com.example.archi.crimefinder.START");
+        String getEnd = intent.getStringExtra("com.example.archi.crimefinder.END");
+        String[] startDateStr = getStart.split("/");
+        int[] startDate = new int[3];
+        for (int i = 0; i < 3; i++) {
+            startDate[i] = Integer.parseInt(startDateStr[i]);
+        }
+        String[] endDateStr = getStart.split("/");
+        int[] endDate = new int[3];
+        for (int i = 0; i < 3; i++) {
+            endDate[i] = Integer.parseInt(endDateStr[i]);
+        }
 
         TextView crimesTitle = (TextView) findViewById(R.id.crimesTitle);
-        String title = "Crimes within " + getRadius + " miles of latitude: " + latitude + " and longitude: " + longitude;
+        String title = "Crimes at latitude: " + latitude + " and longitude: " + longitude + " beginning from: " + getStart + " to: " + getEnd;
         crimesTitle.setText(title);
         Button again_btn  = (Button) findViewById(R.id.again_btn);
         again_btn.setOnClickListener(new View.OnClickListener() {
@@ -34,6 +47,9 @@ public class ResultsActivity extends AppCompatActivity {
                  startActivity(startIntent);
              }
          }
+         String getCrimes = intent.getStringExtra("com.example.archi.crimefinder.CRIMES");
+
+        }
         );
 
 
